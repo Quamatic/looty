0d1b262631480be8047d0f42150434985dfc4f61
Quamatic <lman71037@gmail.com>
2022-12-08 20:35:03 -0600

    Add setConfig

--- lib/init.lua
+++ lib/init.lua
@@ -1,8 +1,11 @@
 local LootPool = require(script.LootPool)
 local LootTable = require(script.LootTable)
 local None = require(script.None)
+local Config = require(script.Config)
 
 return {
+    setConfig = Config.set,
+
     None = None,
     LootPool = LootPool,
     LootTable = LootTable,
